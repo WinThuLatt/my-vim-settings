69bd47edd1d3014e22e0da37df222a96dd0c08fc
win <winthulatt2016@gmail.com>
2024-07-24 00:01:06 +0800

    update batch to create folder

--- set-variables.bat
+++ set-variables.bat
@@ -1,3 +1,9 @@
+mkdir C:\tools\neovim\nvim-win64\config\cache
+mkdir C:\tools\neovim\nvim-win64\config
+mkdir C:\tools\neovim\nvim-win64\config
+mkdir C:\tools\neovim\nvim-win64\config\runtime
+mkdir C:\tools\neovim\nvim-win64\config
+
 setx XDG_CACHE_HOME C:\tools\neovim\nvim-win64\config\cache
 setx XDG_CONFIG_HOME C:\tools\neovim\nvim-win64\config
 setx XDG_DATA_HOME C:\tools\neovim\nvim-win64\config
